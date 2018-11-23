27460696994201419b522d2a24bfd3a6f8932ec1
Pineros Rhon <pierosrh@registrar.ualberta.ca>
2018-11-22 19:34:03 -0700

    Claw controller

---
+++ Chess Robot/claw.py
@@ -0,0 +1,35 @@
+from ev3dev.ev3 import *
+from time import sleep
+
+class Claw:
+    def __init__(self, port = OUTPUT_B):
+        self.motor = MediumMotor(port)
+        #Close the gripper completely
+        convergence_counter = 0
+        time_delta = 0.01
+        tachometer_reading = self.motor.position
+        while convergence_counter < 3:
+            self.motor.run_forever(speed_sp=-360)
+            sleep(time_delta)
+            if tachometer_reading == self.motor.position:
+                convergence_counter = convergence_counter + 1
+            tachometer_reading = self.motor.position
+        self.motor.stop()
+        #Gripper is closed at this point
+        self.motor.run_to_rel_pos(position_sp=700, speed_sp = 360)
+        sleep(3)
+        self.isOpen = True
+
+    def open(self):
+        if not self.isOpen:
+            self.motor.run_to_rel_pos(position_sp=700, speed_sp = 360)
+            sleep(3)
+            self.isOpen = True
+
+    def close(self):
+        if self.isOpen:
+            self.motor.run_to_rel_pos(position_sp=-700, speed_sp = 360)
+            sleep(3)
+            self.isOpen = False
+
+#if __name__ == "__main__":
---
+++ Chess Robot/move.py
@@ -0,0 +1,97 @@
+
+from ev3dev.ev3 import *
+import socket
+from threading import Event, Thread
+from time import sleep
+from claw import *
+
+yMotor = LargeMotor(OUTPUT_A)
+xMotor = LargeMotor(OUTPUT_C)
+zMotor = LargeMotor(OUTPUT_D)
+#gripperMotor = MediumMotor(OUTPUT_B)
+
+
+#Y
+#yMotor.run_to_rel_pos(position_sp=720, speed_sp = 230)
+#sleep(3)
+#yMotor.stop()
+#X
+#xMotor.run_to_rel_pos(position_sp=960, speed_sp = 230)
+#sleep(3)
+#xMotor.stop()
+#Gripper
+#gripperMotor.run_to_rel_pos(position_sp=500, speed_sp = 360)
+#sleep(3)
+#gripperMotor.stop()
+
+#Gripper
+#gripperMotor.run_to_rel_pos(position_sp=-500, speed_sp = 360)
+#sleep(3)
+#gripperMotor.stop()
+
+#Z
+#Upwards
+#zMotor.run_to_rel_pos(position_sp=-120, speed_sp = 100, stop_action="hold")
+##sleep(3)
+zMotor.stop()
+
+
+#Downwards
+
+
+
+
+#gripperMotor.run_to_rel_pos(position_sp=-2000, speed_sp = 360)
+#sleep(6)
+#gripperMotor.stop()
+
+
+#zMotor.run_to_rel_pos(position_sp=-120, speed_sp = 100, stop_action="hold")
+#sleep(3)
+#zMotor.stop()
+
+#xMotor.run_to_rel_pos(position_sp=-960, speed_sp = 230)
+#sleep(3)
+#xMotor.stop()
+
+#yMotor.run_to_rel_pos(position_sp=-420, speed_sp = 230)
+#sleep(3)
+#yMotor.stop()
+
+#zMotor.run_to_rel_pos(position_sp=120, speed_sp = 100, stop_action="coast")
+#sleep(3)
+#zMotor.stop()
+
+
+#gripperMotor.run_to_rel_pos(position_sp=700, speed_sp = 360)
+#sleep(4)
+#gripperMotor.stop()
+
+zMotor.run_to_rel_pos(position_sp=-120, speed_sp = 100, stop_action="hold")
+sleep(3)
+zMotor.stop()
+
+
+claw = Claw(OUTPUT_B)
+
+zMotor.run_to_rel_pos(position_sp=120, speed_sp = 100, stop_action="coast")
+sleep(3)
+zMotor.stop()
+
+claw.close()
+
+
+zMotor.run_to_rel_pos(position_sp=-120, speed_sp = 100, stop_action="hold")
+sleep(3)
+zMotor.stop()
+
+yMotor.run_to_rel_pos(position_sp=-420, speed_sp = 230)
+sleep(3)
+yMotor.stop()
+
+zMotor.run_to_rel_pos(position_sp=120, speed_sp = 100, stop_action="coast")
+sleep(3)
+zMotor.stop()
+
+claw.open()
+#zMotor.reset()
