13d8d8194d3f61dcee0a6ea7e36fc8b78c4c7296
bbest31 <brandonbest94@live.ca>
2018-11-29 20:25:43 -0700

    Robot pickup and place routine done

--- Chess Robot/source/server.py
+++ Chess Robot/source/server.py
@@ -13,7 +13,7 @@ class Server:
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         #We need to use the ip address that shows up in ipconfig for the usb ethernet adapter
         #That handles the comunication between the PC and the crick
-        host = "169.254.137.85"
+        host = "169.254.82.210"
         print ("host: ", host)                        
         port = 9999
         serversocket.bind((host, port))                                  
@@ -29,11 +29,11 @@ class Server:
     #       joint_angle [Float]: The angle by which we want to joint to move
     #       queue [Thread-safe Queue]: Mutable data structure to store (and return)
     #             the messages received from the client
-    def sendAngles(self, x_motor_angle, y_motor_angle, queue):
+    def sendDistances(self, x_distance, y_distance, queue):
         #Format in which the client expects the data
         # angle1    angle2
-        print(str(x_motor_angle) +  " " + str(y_motor_angle))
-        data = str(x_motor_angle)+","+str(y_motor_angle)
+        print(str(x_distance) +  " " + str(y_distance))
+        data = str(x_distance)+","+str(y_distance)
         print("Sending Data: (" + data + ") to robot.")
         self.cs.send(data.encode("UTF-8"))
         #Waiting for the client (ev3 brick) to let the server know
@@ -48,6 +48,19 @@ class Server:
 
     def sendLowerClaw(self):
         self.cs.send("LOWER_CLAW".encode("UTF-8"))
+        time.sleep(3.5)
+    
+    def sendRaiseClaw(self):
+        self.cs.send("RAISE_CLAW".encode("UTF-8"))
+        time.sleep(3.5)
+
+    def sendOpenClaw(self):
+        self.cs.send("OPEN_CLAW".encode("UTF-8"))
+        time.sleep(3.5)
+
+    def sendCloseClaw(self):
+        self.cs.send("CLOSE_CLAW".encode("UTF-8"))
+        time.sleep(3.5)
 
 
 
