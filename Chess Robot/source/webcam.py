7f398545a7635fb36f6456029ecb3e3e985257bc
phebert5009 <cpierrot273@gmail.com>
2018-12-03 16:28:15 -0700

    attempt at preventing camera freeze

--- Chess Robot/source/webcam.py
+++ Chess Robot/source/webcam.py
@@ -9,7 +9,7 @@ class Webcam:
           
     # create thread for capturing images
     def start(self):
-        Thread(target=self._update_frame, args=()).start()
+        self.thread = Thread(target=self._update_frame, args=()).start()
   
     def _update_frame(self):
         while(True):
