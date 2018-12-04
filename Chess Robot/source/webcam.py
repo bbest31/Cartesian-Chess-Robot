b572331f7ff76fbc1feb7118e8ae1e24cb92d747
phebert5009 <cpierrot273@gmail.com>
2018-12-03 17:22:52 -0700

    self stopping thread

--- Chess Robot/source/webcam.py
+++ Chess Robot/source/webcam.py
@@ -6,13 +6,17 @@ class Webcam:
     def __init__(self):
         self.video_capture = cv2.VideoCapture(0)
         self.current_frame = self.video_capture.read()[1]
+        self.__continue = True
+        
+    def __del__(self):
+        self.__continue = False
           
     # create thread for capturing images
     def start(self):
         self.thread = Thread(target=self._update_frame, args=()).start()
   
     def _update_frame(self):
-        while(True):
+        while(self.__continue):
             self.current_frame = self.video_capture.read()[1]
                   
     # get the current frame
