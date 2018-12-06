e30d60e258a521da7ef531e924c2c5cbc39b6d5e
phebert5009 <cpierrot273@gmail.com>
2018-12-05 19:30:35 -0700

    modified clawDown in an attempt to calibrate it

--- Chess Robot/source/robot.py
+++ Chess Robot/source/robot.py
@@ -78,7 +78,7 @@ class Robot:
         self.zMotor.stop()
 
     def clawDown(self):
-        self.zMotor.run_to_rel_pos(position_sp=170, speed_sp = 50, stop_action="coast")
+        self.zMotor.run_to_rel_pos(position_sp=100, speed_sp = 50, stop_action="coast")
         sleep(3.5)
         self.zMotor.stop()
 
